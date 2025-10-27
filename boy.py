d084b45fa523f33f22e8371898ae3b0ff0a151fc
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-10-27 12:28:55 +0900

    In_Class Commit(4)

--- boy.py
+++ boy.py
@@ -138,7 +138,7 @@ class Boy:
 
     def fire_ball(self):
         ball = Ball(self.x, self.y, self.face_dir * 10)
-        game_world.add_object(ball)
+        game_world.add_object(ball, 1)
         if self.face_dir == -1:
             print('FIRE BALL TO LEFT')
         elif self.face_dir == 1:
--- game_world.py
+++ game_world.py
@@ -1,15 +1,31 @@
 # 게임 내의 객체들의 생성과 소멸을 관리하는 모듈입니다.
 
-world = []
+# layer 0: Background Objects
+# layer 1: Foreground Objects
 
-def add_object(o):
-    world.append(o)
+world = [[], []]
+
+def add_object(o, depth = 0):
+    world[depth].append(o)
+
+# 게임 월드에 객체 리스트를 추가하는 함수
+def add_objects(ol, depth = 0):
+    world[depth] += ol
 
 def update():
-    for o in world: o.update()
+    for layer in world:
+        for o in layer:
+            o.update()
 
 def render():
-    for o in world: o.draw()
+    for layer in world:
+        for o in layer:
+            o.draw()
 
 def remove_object(o):
-    world.remove(o)
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+
+    raise ValueError('월드에 존재하지 않는 객체를 삭제하려 함.!.!')
